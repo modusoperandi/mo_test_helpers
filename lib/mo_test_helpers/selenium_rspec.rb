9b278f602396ce49a1e1217a4a471348ea74ed11
Max Schulze <max@mo-stud.io>
2013-01-14 08:41:59 +0100

    remove driver init, can't access in rspec

--- lib/mo_test_helpers/selenium_rspec.rb
+++ lib/mo_test_helpers/selenium_rspec.rb
@@ -1,9 +1,3 @@
 require "selenium-webdriver"
 require 'pp'
 require 'mo_test_helpers/selenium_helper'
-
-@driver = SeleniumHelper.grid_selenium_browser
-
-unless @driver
-  raise ArgumentError.new('Driver not found!')
-end
